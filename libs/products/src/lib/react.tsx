03ca3b9b2620b899f0ec316a9ca6033b7c49e665
Jose Javier Martinez <mjosejavier15@gmail.com>
2024-04-22 21:08:50 -0400

    feat: added react lib

---
+++ libs/products/src/lib/react.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ReactProps {}
+
+export function React(props: ReactProps) {
+  return (
+    <div>
+      <h1>This is a react app</h1>
+    </div>
+  );
+}
+
+export default React;
