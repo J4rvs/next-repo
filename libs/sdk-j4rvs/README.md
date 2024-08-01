44aa1cfe860031e6c09957c04ba5e3b239562c8f
Jose Javier Martinez <mjosejavier15@gmail.com>
2024-07-31 21:59:09 -0400

    feat: add lib

---
+++ libs/sdk-j4rvs/README.md
@@ -0,0 +1,7 @@
+# sdk-j4rvs
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test sdk-j4rvs` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/sdk-j4rvs/src/lib/sdk-j4rvs.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface SdkJ4rvsProps {}
+
+export function SdkJ4rvs(props: SdkJ4rvsProps) {
+  return (
+    <div>
+      <h1>Welcome to SdkJ4rvs!</h1>
+    </div>
+  );
+}
+
+export default SdkJ4rvs;
