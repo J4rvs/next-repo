2d6407ae317044a3ad99fabc666c965e2804d87f
Jose Javier Martinez <mjosejavier15@gmail.com>
2024-08-05 16:50:03 -0400

    feat: change
     Changing component locomotora

--- libs/locomotora/src/lib/locomotora.tsx
+++ libs/locomotora/src/lib/locomotora.tsx
@@ -4,7 +4,7 @@ export interface LocomotoraProps {}
 export function Locomotora(props: LocomotoraProps) {
   return (
     <div>
-      <h1>Welcome to Locomotora!</h1>
+      <h1>Welcome to Locomotorza!</h1>
     </div>
   );
 }
